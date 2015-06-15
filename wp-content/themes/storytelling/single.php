ef36eb374fbaca0929e4c0bfe0029546148ccdaf
henrikhelsingborg <henrik.wilhelmsson@helsingborg.se>
2015-06-15 13:38:15 +0200

    various mobile tweaks

--- wp-content/themes/storytelling/single.php
+++ wp-content/themes/storytelling/single.php
@@ -18,17 +18,22 @@ remove_action( 'omega_after_main', 'omega_primary_sidebar' );
 
 <!--	<main class="<?php echo omega_apply_atomic( 'main_class', 'content' );?>" <?php omega_attr( 'content' ); ?>> -->
 <main class="post-content" itemtype="http://schema.org/Blog" itemscope="itemscope" role="main">
-		<a href="/" class="post-home-link" title="Till startsidan">&lt; Till startsidan</a>
-		<?php
-		if ( has_post_thumbnail() ) {
-				the_post_thumbnail();
-		} ?>
+	<a href="/" class="post-home-link" title="Till startsidan">&lt; Till startsidan</a>
+	<div class="hbgredlogo"><img src="/wp-content/themes/storytelling/images/hbg_vapen.png" alt=""/></div>
+		<div class="storyimage">
+				<?php
+			if ( has_post_thumbnail() ) {
+					the_post_thumbnail();
+				} ?>
+		</div>
+
 		<!-- Added social media -->
 		<ul class="socialmedia-list">
 			<li class="fbook"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>"><img src="/wp-content/themes/storytelling/images/facebook.png" alt="Facebook"/></a></li>
 			<li></li>
 			<li class="twitter"><a href="http://twitter.com/share?url=<?php echo urlencode(wp_get_shortlink()); ?>"><img src="/wp-content/themes/storytelling/images/twitter.png" alt="Twitter"/></a></li>
 		</ul>
+
 
 		<!-- Added left sidebar -->
 		<div id="left-widget-area" class="widget-area" role="complementary">
